974e49a5dbe022aee361b4d55a8cc010092b66f8
jcvalenzuela <124800557+jcvalenzuela@users.noreply.github.com>
2023-03-15 23:08:22 +0800

    Commit Info:
    -Generate test init test rules

---
+++ GCashWeatherApp/app/src/test/java/com/jcvalenzuela/gcashweatherapp/helper/utils/UtilityTest.java
@@ -0,0 +1,67 @@
+package com.jcvalenzuela.gcashweatherapp.helper.utils;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import org.junit.Test;
+
+public class UtilityTest {
+
+
+    @Test
+    public void isValidMail() {
+    }
+
+    @Test
+    public void isStringEmpty() {
+    }
+
+    @Test
+    public void disposeComposite() {
+    }
+
+    @Test
+    public void dispose() {
+    }
+
+    @Test
+    public void disposeAlertDialog() {
+    }
+
+    @Test
+    public void dateAndTimeFormatter() {
+    }
+
+    @Test
+    public void convertDblToStr() {
+    }
+
+    @Test
+    public void convertToDate() {
+    }
+
+    @Test
+    public void convertToTime() {
+    }
+
+    @Test
+    public void isSunset() {
+    }
+
+    @Test
+    public void getCurrentWeatherStatus() {
+    }
+
+    @Test
+    public void isDayTime() {
+    }
+
+    @Test
+    public void getWeatherStatus() {
+    }
+
+    @Test
+    public void setWeatherIconByTime() {
+    }
+}
